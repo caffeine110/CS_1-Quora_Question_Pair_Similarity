1eac323ee7226d602432009388447c78b1404272
caffeine110 <gaurav.developer.audio@gmail.com>
2019-09-10 20:24:00 +0530

    excctract features.

---
+++ FeatureExctraction/featureExctraction.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 10 19:59:57 2019
+
+@author: gaurav
+
+    inFile  : ../data/train/nlp_features_train.csv
+            : ../data/train/train.csv
+
+    outFile : ../data/train/final_features.csv
+
+
+"""
+
+
+
+import pandas as pd
+import numpy as np
+import sys
+import os 
+import spacy
+
+import re
+import time
+import warnings
+warnings.filterwarnings("ignore")
+
+from nltk.corpus import stopwords
+from sklearn.preprocessing import normalize
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from tqdm import tqdm
+
+
+
+
+
+# exctract word2vec vectors
+# https://github.com/explosion/spaCy/issues/1721
+# http://landinghub.visualstudio.com/visual-cpp-build-tools
+
+
+
+
+# filePath to open
+filePath = "../data/train/train.csv"
+df = pd.read_csv(filePath)
+ 
+df['question1'] = df['question1'].apply(lambda x: str(x))
+df['question2'] = df['question2'].apply(lambda x: str(x))
+
+
+
+# Processing
+# vectorizinng
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+# merge texts
+questions = list(df['question1']) + list(df['question2'])
+
+tfidf = TfidfVectorizer(lowercase=False, )
+tfidf.fit_transform(questions)
+
+# dict key:word and value:tf-idf score
+word2tfidf = dict(zip(tfidf.get_feature_names(), tfidf.idf_))
+
+
+
+
+
+# en_vectors_web_lg, which includes over 1 million unique vectors.
+nlp = spacy.load('en_core_web_sm')
+
+vecs1 = []
+# https://github.com/noamraph/tqdm
+# tqdm is used to print the progress bar
+for qu1 in tqdm(list(df['question1'])):
+    doc1 = nlp(qu1) 
+    # 384 is the number of dimensions of vectors 
+    mean_vec1 = np.zeros([len(doc1), len(doc1[0].vector)])
+    for word1 in doc1:
+        # word2vec
+        vec1 = word1.vector
+        # fetch df score
+        try:
+            idf = word2tfidf[str(word1)]
+        except:
+            idf = 0
+        # compute final vec
+        mean_vec1 += vec1 * idf
+    mean_vec1 = mean_vec1.mean(axis=0)
+    vecs1.append(mean_vec1)
+df['q1_feats_m'] = list(vecs1)
+
+
+
+
+
+vecs2 = []
+for qu2 in tqdm(list(df['question2'])):
+    doc2 = nlp(qu2) 
+    mean_vec1 = np.zeros([len(doc1), len(doc2[0].vector)])
+    for word2 in doc2:
+        # word2vec
+        vec2 = word2.vector
+        # fetch df score
+        try:
+            idf = word2tfidf[str(word2)]
+        except:
+            #print word
+            idf = 0
+        # compute final vec
+        mean_vec2 += vec2 * idf
+    mean_vec2 = mean_vec2.mean(axis=0)
+    vecs2.append(mean_vec2)
+df['q2_feats_m'] = list(vecs2)
+
+
+
+
+
+#prepro_features_train.csv (Simple Preprocessing Feartures)
+#nlp_features_train.csv (NLP Features)
+if os.path.isfile('../data/train/nlp_features_train.csv'):
+    dfnlp = pd.read_csv("../data/train/nlp_features_train.csv",encoding='latin-1')
+else:
+    print("download nlp_features_train.csv from drive or run previous notebook")
+
+if os.path.isfile('../data/train/df_fe_without_preprocessing_train.csv'):
+    dfppro = pd.read_csv("../data/train/df_fe_without_preprocessing_train.csv",encoding='latin-1')
+else:
+    print("download df_fe_without_preprocessing_train.csv from drive or run previous notebook")
+    
+    
+    
+        
+df1 = dfnlp.drop(['qid1','qid2','question1','question2'],axis=1)
+df2 = dfppro.drop(['qid1','qid2','question1','question2','is_duplicate'],axis=1)
+df3 = df.drop(['qid1','qid2','question1','question2','is_duplicate'],axis=1)
+df3_q1 = pd.DataFrame(df3.q1_feats_m.values.tolist(), index= df3.index)
+df3_q2 = pd.DataFrame(df3.q2_feats_m.values.tolist(), index= df3.index)
+
+
+
+
+print("Number of features in nlp dataframe :", df1.shape[1])
+print("Number of features in preprocessed dataframe :", df2.shape[1])
+print("Number of features in question1 w2v  dataframe :", df3_q1.shape[1])
+print("Number of features in question2 w2v  dataframe :", df3_q2.shape[1])
+print("Number of features in final dataframe  :", df1.shape[1]+df2.shape[1]+df3_q1.shape[1]+df3_q2.shape[1])
+
+
+
+# storing the final features to csv file
+if not os.path.isfile('../data/train/final_features.csv'):
+    df3_q1['id']=df1['id']
+    df3_q2['id']=df1['id']
+    df1  = df1.merge(df2, on='id',how='left')
+    df2  = df3_q1.merge(df3_q2, on='id',how='left')
+    result  = df1.merge(df2, on='id',how='left')
+    result.to_csv('../data/train/final_features.csv')
+    
+    
+    
+    
+print("EOF...")
